c01aa8dea289bb2ad4686543bf8fff2980769bd3
natella <natella@buhta.ru>
2014-12-04 13:43:04 +0400

    no comment

--- www/modules/buhta/core/datatypes/DateTime.ts
+++ www/modules/buhta/core/datatypes/DateTime.ts
@@ -1,25 +1,29 @@
 ﻿/// <reference path="../../../../lib/lib.ts" />
 
+interface Moment {
+    dayOfYear(): number;  // глюк в библиотеке typescript для moment
+}
+
 class DateTime {
 
-    moment: Moment;
+    moment:Moment;
 
     constructor();
-    constructor(date: Date);
-    constructor(date: number);
-    constructor(date: number[]);
-    constructor(date: string, format?: string, strict?: boolean);
-    constructor(date: string, format?: string, language?: string, strict?: boolean);
-    constructor(date: string, formats: string[], strict?: boolean);
-    constructor(date: string, formats: string[], language?: string, strict?: boolean);
-    constructor(date: string, specialFormat: () => void, strict?: boolean);
-    constructor(date: string, specialFormat: () => void, language?: string, strict?: boolean);
-    constructor(date: string, formatsIncludingSpecial: any[], strict?: boolean);
-    constructor(date: string, formatsIncludingSpecial: any[], language?: string, strict?: boolean);
-    constructor(date: Date);
-    constructor(date: Object);
+    constructor(date:Date);
+    constructor(date:number);
+    constructor(date:number[]);
+    constructor(date:string, format?:string, strict?:boolean);
+    constructor(date:string, format?:string, language?:string, strict?:boolean);
+    constructor(date:string, formats:string[], strict?:boolean);
+    constructor(date:string, formats:string[], language?:string, strict?:boolean);
+    constructor(date:string, specialFormat:() => void, strict?:boolean);
+    constructor(date:string, specialFormat:() => void, language?:string, strict?:boolean);
+    constructor(date:string, formatsIncludingSpecial:any[], strict?:boolean);
+    constructor(date:string, formatsIncludingSpecial:any[], language?:string, strict?:boolean);
+    constructor(date:Date);
+    constructor(date:Object);
 
-    constructor(p1?: any, p2?: any, p3?: any, p4?: any) {
+    constructor(p1?:any, p2?:any, p3?:any, p4?:any) {
         if (p1 && p1.length && p1.length >= 2 && p1.length <= 7) {
             // у этих дебилов месяцы начинаются с нуля, исправляем
             if (p1[1]) {
@@ -28,23 +32,188 @@ class DateTime {
         }
         this.moment = moment(p1, p2, p3, p4);
     }
-    toString(format?: string) {
+
+    toString(format?:string) {
         return this.moment.format(format);
     }
 
-    toSql(): string {
+    toSql():string {
         return this.toString("'YYYY-MM-DD HH:mm:ss.SS'");
     }
 
-    toTime(): Time {
+    toTime():Time {
         return new Time(this.toString("HH:mm:ss.SS"));
     }
 
     // из momentjs
-    fromNow(withoutSuffix?: boolean): string { return this.moment.fromNow(withoutSuffix) }
-    startOf(unitOfTime: string): DateTime { return new DateTime(this.moment.startOf(unitOfTime).toDate()) }
-    endOf(unitOfTime: string): DateTime { return new DateTime(this.moment.endOf(unitOfTime).toDate()) }
-    add(unitOfTime: string, amount: number): DateTime { return new DateTime(this.moment.add(unitOfTime, amount).toDate()) }
+    fromNow(withoutSuffix?:boolean):string {
+        return this.moment.fromNow(withoutSuffix)
+    }
+
+    startOf(unitOfTime:string):DateTime {
+        return new DateTime(this.moment.startOf(unitOfTime).toDate())
+    }
+
+    endOf(unitOfTime:string):DateTime {
+        return new DateTime(this.moment.endOf(unitOfTime).toDate())
+    }
+
+    add(unitOfTime:string, amount:number):DateTime {
+        return new DateTime(this.moment.add(unitOfTime, amount).toDate())
+    }
+
+    addYears(years:number):DateTime {
+        return new DateTime(this.moment.clone().add(years, 'years').toDate());
+    }
+
+    addQuarters(quarters:number):DateTime {
+        return new DateTime(this.moment.clone().add(quarters, 'quarters').toDate());
+    }
+
+    addMonths(months:number):DateTime {
+        return new DateTime(this.moment.clone().add(months, 'months').toDate());
+    }
+
+    addWeeks(weeks:number):DateTime {
+        return new DateTime(this.moment.clone().add(weeks, 'weeks').toDate());
+    }
+
+    addDays(days:number):DateTime {
+        return new DateTime(this.moment.clone().add(days, 'days').toDate());
+    }
+
+    addHours(hours:number):DateTime {
+        return new DateTime(this.moment.clone().add(hours, 'hours').toDate());
+    }
+
+    addMinutes(minutes:number):DateTime {
+        return new DateTime(this.moment.clone().add(minutes, 'minutes').toDate());
+    }
+
+    addSeconds(seconds:number):DateTime {
+        return new DateTime(this.moment.clone().add(seconds, 'seconds').toDate());
+    }
+
+    addMilliseconds(milliseconds:number):DateTime {
+        return new DateTime(this.moment.clone().add(milliseconds, 'milliseconds').toDate());
+    }
+
+    startOfYear():DateTime {
+        return new DateTime(this.moment.clone().startOf('year').toDate());
+    }
+
+    startOfQuarter():DateTime {
+        return new DateTime(this.moment.clone().startOf('quarter').toDate());
+    }
+
+    startOfMonth():DateTime {
+        return new DateTime(this.moment.clone().startOf('month').toDate());
+    }
+
+    startOfWeek():DateTime {
+        return new DateTime(this.moment.clone().startOf('isoWeek').toDate());
+    }
+
+    startOfDay():DateTime {
+        return new DateTime(this.moment.clone().startOf('day').toDate());
+    }
+
+    startOfHour():DateTime {
+        return new DateTime(this.moment.clone().startOf('hour').toDate());
+    }
+
+    startOfMinute():DateTime {
+        return new DateTime(this.moment.clone().startOf('minute').toDate());
+    }
+
+    startOfSecond():DateTime {
+        return new DateTime(this.moment.clone().startOf('second').toDate());
+    }
+
+    endOfYear():DateTime {
+        return new DateTime(this.moment.clone().endOf('year').toDate());
+    }
+
+    endOfQuarter():DateTime {
+        return new DateTime(this.moment.clone().endOf('quarter').toDate());
+    }
+
+    endOfMonth():DateTime {
+        return new DateTime(this.moment.clone().endOf('month').toDate());
+    }
+
+    endOfWeek():DateTime {
+        return new DateTime(this.moment.clone().endOf('isoWeek').toDate());
+    }
+
+    endOfDay():DateTime {
+        return new DateTime(this.moment.clone().endOf('day').toDate());
+    }
+
+    endOfHour():DateTime {
+        return new DateTime(this.moment.clone().endOf('hour').toDate());
+    }
+
+    endOfMinute():DateTime {
+        return new DateTime(this.moment.clone().endOf('minute').toDate());
+    }
+
+    endOfSecond():DateTime {
+        return new DateTime(this.moment.clone().endOf('second').toDate());
+    }
+
+    year():number {
+        return this.moment.clone().year().toNumber();
+    }
+
+    quarter():number {
+        return this.moment.clone().quarter().toNumber();
+    }
+
+    month():number {
+        return (this.moment.clone().month().toNumber() + 1);
+    }
+
+    week():number {
+        return this.moment.clone().isoWeek().toNumber();
+    }
+
+    day():number {
+        return this.moment.clone().date().toNumber();
+    }
+
+    hour():number {
+        return this.moment.clone().hour().toNumber();
+    }
+
+    minute():number {
+        return this.moment.clone().minute().toNumber();
+    }
+
+    second():number {
+        return this.moment.clone().second().toNumber();
+    }
+
+    millisecond():number {
+        return this.moment.clone().millisecond().toNumber();
+    }
+
+    dayOfWeek():number {
+        return this.moment.clone().isoWeekday().toNumber();
+    }
+
+    dayOfYear():number {
+        return this.moment.clone().dayOfYear().toNumber();
+    }
+
+    daysInMonth():number {
+        return this.moment.clone().daysInMonth().toNumber();
+    }
+
+    // 1 день = 24 часа
+    diffDays(datetime:DateTime):number {
+        return this.moment.clone().diff(datetime.moment.clone(), 'days').toNumber();
+    }
 
 }
 
--- www/modules/buhta/tests/TestDateTime/TestDateTime.ts
+++ www/modules/buhta/tests/TestDateTime/TestDateTime.ts
@@ -86,6 +86,113 @@ module BuhtaTestModule {
             var t = dt.toTime();
             this.areIdentical(t.toSql(), "'16:39:21.02'", "DateTime.toTime()");
         }
+
+        test_add() {
+            //  add меняют оригинальную дату !!!
+
+            var dt = new DateTime([2006, 4, 30, 16, 39, 21, 23]);
+            this.areIdentical(dt.addDays(1).toSql(), new DateTime([2006, 5, 1, 16, 39, 21, 23]).toSql(), "add days +1");
+            this.areIdentical(dt.addDays(-1).toSql(), new DateTime([2006, 4, 29, 16, 39, 21, 23]).toSql(), "add days -1");
+            this.areIdentical(dt.addDays(365).toSql(), new DateTime([2007, 4, 30, 16, 39, 21, 23]).toSql(), "add days 365");
+            this.areIdentical(dt.addDays(-365).toSql(), new DateTime([2005, 4, 30, 16, 39, 21, 23]).toSql(), "add days -365");
+
+            this.areIdentical(dt.addYears(1).toSql(), new DateTime([2007, 4, 30, 16, 39, 21, 23]).toSql(), "add years +1");
+            this.areIdentical(dt.addYears(-10).toSql(), new DateTime([1996, 4, 30, 16, 39, 21, 23]).toSql(), "add years -10");
+
+            var dt = new DateTime([2006, 5, 31, 16, 39, 21, 23]);
+            this.areIdentical(dt.addQuarters(2).toSql(), new DateTime([2006, 11, 30, 16, 39, 21, 23]).toSql(), "add quarters +2");
+            this.areIdentical(dt.addQuarters(-1).toSql(), new DateTime([2006, 2, 28, 16, 39, 21, 23]).toSql(), "add quarters -1");
+            this.areIdentical(dt.addQuarters(4).toSql(), new DateTime([2007, 5, 31, 16, 39, 21, 23]).toSql(), "add quarters 4");
+            this.areIdentical(dt.addQuarters(-4).toSql(), new DateTime([2005, 5, 31, 16, 39, 21, 23]).toSql(), "add quarters -4");
+
+            var dt = new DateTime([2006, 3, 31, 16, 39, 21, 23]);
+            this.areIdentical(dt.addMonths(1).toSql(), new DateTime([2006, 4, 30, 16, 39, 21, 23]).toSql(), "add months +1");
+            this.areIdentical(dt.addMonths(-1).toSql(), new DateTime([2006, 2, 28, 16, 39, 21, 23]).toSql(), "add months -1");
+            this.areIdentical(dt.addMonths(12).toSql(), new DateTime([2007, 3, 31, 16, 39, 21, 23]).toSql(), "add months 12");
+            this.areIdentical(dt.addMonths(-12).toSql(), new DateTime([2005, 3, 31, 16, 39, 21, 23]).toSql(), "add months -12");
+
+            var dt = new DateTime([2006, 4, 30, 16, 39, 21, 23]);
+            this.areIdentical(dt.addWeeks(1).toSql(), new DateTime([2006, 5, 7, 16, 39, 21, 23]).toSql(), "add weeks +1");
+            this.areIdentical(dt.addWeeks(-1).toSql(), new DateTime([2006, 4, 23, 16, 39, 21, 23]).toSql(), "add weeks -1");
+            this.areIdentical(dt.addWeeks(5).toSql(), new DateTime([2006, 6, 4, 16, 39, 21, 23]).toSql(), "add weeks 5");
+            this.areIdentical(dt.addWeeks(-5).toSql(), new DateTime([2006, 3, 26, 16, 39, 21, 23]).toSql(), "add weeks -5");
+
+            this.areIdentical(dt.addHours(1).toSql(), new DateTime([2006, 4, 30, 17, 39, 21, 23]).toSql(), "add hours +1");
+            this.areIdentical(dt.addHours(-1).toSql(), new DateTime([2006, 4, 30, 15, 39, 21, 23]).toSql(), "add hours -1");
+            this.areIdentical(dt.addHours(24).toSql(), new DateTime([2006, 5, 1, 16, 39, 21, 23]).toSql(), "add hours 24");
+            this.areIdentical(dt.addHours(-24).toSql(), new DateTime([2006, 4, 29, 16, 39, 21, 23]).toSql(), "add hours -24");
+
+            this.areIdentical(dt.addMinutes(1).toSql(), new DateTime([2006, 4, 30, 16, 40, 21, 23]).toSql(), "add minutes +1");
+            this.areIdentical(dt.addMinutes(-1).toSql(), new DateTime([2006, 4, 30, 16, 38, 21, 23]).toSql(), "add minutes -1");
+            this.areIdentical(dt.addMinutes(60).toSql(), new DateTime([2006, 4, 30, 17, 39, 21, 23]).toSql(), "add minutes 60");
+            this.areIdentical(dt.addMinutes(-60).toSql(), new DateTime([2006, 4, 30, 15, 39, 21, 23]).toSql(), "add minutes -60");
+
+            this.areIdentical(dt.addSeconds(1).toSql(), new DateTime([2006, 4, 30, 16, 39, 22, 23]).toSql(), "add seconds +1");
+            this.areIdentical(dt.addSeconds(-1).toSql(), new DateTime([2006, 4, 30, 16, 39, 20, 23]).toSql(), "add seconds -1");
+            this.areIdentical(dt.addSeconds(60).toSql(), new DateTime([2006, 4, 30, 16, 40, 21, 23]).toSql(), "add seconds 60");
+            this.areIdentical(dt.addSeconds(-60).toSql(), new DateTime([2006, 4, 30, 16, 38, 21, 23]).toSql(), "add seconds -60");
+
+            this.areIdentical(dt.addMilliseconds(1).toSql(), new DateTime([2006, 4, 30, 16, 39, 21, 24]).toSql(), "add milliseconds +1");
+            this.areIdentical(dt.addMilliseconds(-1).toSql(), new DateTime([2006, 4, 30, 16, 39, 21, 22]).toSql(), "add milliseconds -1");
+            this.areIdentical(dt.addMilliseconds(1000).toSql(), new DateTime([2006, 4, 30, 16, 39, 22, 23]).toSql(), "add milliseconds 1000");
+            this.areIdentical(dt.addMilliseconds(-1000).toSql(), new DateTime([2006, 4, 30, 16, 39, 20, 23]).toSql(), "add milliseconds -1000");
+
+        }
+
+        test_startOf_endOf() {
+            //  startOf_endOf меняют оригинальную дату !!!
+            var dt = new DateTime([2006, 4, 30, 16, 39, 21, 23]);
+            this.areIdentical(dt.startOfYear().toSql(), new DateTime([2006, 1, 1, 0, 0, 0, 0]).toSql(), "startOf Year");
+            this.areIdentical(dt.startOfQuarter().toSql(), new DateTime([2006, 4, 1, 0, 0, 0, 0]).toSql(), "startOf Quarter");
+            this.areIdentical(dt.startOfMonth().toSql(), new DateTime([2006, 4, 1, 0, 0, 0, 0]).toSql(), "startOf Month");
+            this.areIdentical(dt.startOfWeek().toSql(), new DateTime([2006, 4, 24, 0, 0, 0, 0]).toSql(), "startOf Week");
+            this.areIdentical(dt.startOfDay().toSql(), new DateTime([2006, 4, 30, 0, 0, 0, 0]).toSql(), "startOf Day");
+            this.areIdentical(dt.startOfHour().toSql(), new DateTime([2006, 4, 30, 16, 0, 0, 0]).toSql(), "startOf Hour");
+            this.areIdentical(dt.startOfMinute().toSql(), new DateTime([2006, 4, 30, 16, 39, 0, 0]).toSql(), "startOf Minute");
+            this.areIdentical(dt.startOfSecond().toSql(), new DateTime([2006, 4, 30, 16, 39, 21, 0]).toSql(), "startOf Second");
+            this.areIdentical(dt.endOfDay().toSql(), new DateTime([2006, 4, 30, 23, 59, 59, 999]).toSql(), "endOf Day");
+            this.areIdentical(dt.endOfHour().toSql(), new DateTime([2006, 4, 30, 16, 59, 59, 999]).toSql(), "endOf Hour");
+            this.areIdentical(dt.endOfMinute().toSql(), new DateTime([2006, 4, 30, 16, 39, 59, 999]).toSql(), "endOf Minute");
+            this.areIdentical(dt.endOfSecond().toSql(), new DateTime([2006, 4, 30, 16, 39, 21, 999]).toSql(), "endOf Second");
+
+            var dt = new DateTime([2006, 4, 29, 16, 39, 21, 23]);
+            this.areIdentical(dt.endOfYear().toSql(), new DateTime([2006, 12, 31, 23, 59, 59, 999]).toSql(), "endOf Year");
+            this.areIdentical(dt.endOfQuarter().toSql(), new DateTime([2006, 6, 30, 23, 59, 59, 999]).toSql(), "endOf Quarter");
+            this.areIdentical(dt.endOfMonth().toSql(), new DateTime([2006, 4, 30, 23, 59, 59, 999]).toSql(), "endOf Month");
+            this.areIdentical(dt.endOfWeek().toSql(), new DateTime([2006, 4, 30, 23, 59, 59, 999]).toSql(), "endOf Week");
+        }
+
+        test_get_DateTime() {
+            //  get_DateTime получить час-мин-сек-день-месяц-год !!!
+            var dt = new DateTime([2006, 4, 30, 16, 39, 21, 23]);
+            this.areIdentical(dt.year().toString(), "2006", "get_DateTime year");
+            this.areIdentical(dt.quarter().toString(), "2", "get_DateTime quarter");
+            this.areIdentical(dt.month().toString(), "4", "get_DateTime month");
+            this.areIdentical(dt.day().toString(), "30", "get_DateTime day");
+            this.areIdentical(dt.hour().toString(), "16", "get_DateTime hour");
+            this.areIdentical(dt.minute().toString(), "39", "get_DateTime minute");
+            this.areIdentical(dt.second().toString(), "21", "get_DateTime second");
+            this.areIdentical(dt.millisecond().toString(), "23", "get_DateTime millisecond");
+
+            var dt = new DateTime([2014, 1, 1, 16, 39, 21, 23]);
+            this.areIdentical(dt.week().toString(), "1", "get_DateTime week");
+            this.areIdentical(dt.dayOfWeek().toString(), "3", "get_DateTime dayOfWeek");
+            this.areIdentical(dt.dayOfYear().toString(), "1", "get_DateTime dayOfYear");
+            var dt = new DateTime([2014, 12, 27, 16, 39, 21, 23]);
+            this.areIdentical(dt.week().toString(), "52", "get_DateTime week");
+            this.areIdentical(dt.dayOfWeek().toString(), "6", "get_DateTime dayOfWeek");
+            this.areIdentical(dt.dayOfYear().toString(), "361", "get_DateTime dayOfYear");
+            this.areIdentical(dt.daysInMonth().toString(), "31", "get_DateTime daysInMonth");
+
+        }
+
+        test_diff_DateTime() {
+            //  diff_DateTime разница между датами  в днях-месяцах-годах !!!
+            var dt1 = new DateTime([2014, 4, 21, 0, 0, 0, 0]);
+            var dt2 = new DateTime([2014, 4, 20, 0, 0, 0, 0]);
+            this.areIdentical(dt1.diffDays(dt2).toString(), "1", "diff_DateTime day");
+        }
+
     }
 
 }
